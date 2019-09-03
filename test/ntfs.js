2171c741d9f357af049c06dd15a0d1a3609c9b27
Jonas Hermsmeier <jhermsmeier@gmail.com>
2019-09-03 23:47:36 +0200

    test(guid): Add test for GUID.from()

--- test/ntfs.js
+++ test/ntfs.js
@@ -4,4 +4,16 @@ var inspect = require( './inspect' )
 
 context( 'NTFS', function() {
   
+  context( 'GUID', function() {
+    
+    specify( '.from()', function() {
+      var value = '6B29FC40-CA47-1067-B31D-00DD010662DA'
+      var guid = NTFS.GUID.from( value )
+      inspect.log( guid.parts )
+      inspect.log( guid )
+      assert.strictEqual( guid.toString(), value )
+    })
+    
+  })
+  
 })
